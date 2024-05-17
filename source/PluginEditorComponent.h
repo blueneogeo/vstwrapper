f76d758631848622cc796ae2412c5bb11232c4dd
Christian Vogel <chris.vogel@me.com>
2024-05-17 13:15:52 +0200

    remove unused import

--- source/PluginEditorComponent.h
+++ source/PluginEditorComponent.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #include <juce_audio_processors/juce_audio_processors.h>
-#include "EditorTools.h"
 #include "juce_gui_basics/juce_gui_basics.h"
 
 class PluginEditorComponent final : public juce::Component
