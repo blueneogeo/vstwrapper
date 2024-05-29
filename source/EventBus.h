2892c6f0e57f5fa0eaf4635446fd8aeec20208fe
Christian Vogel <chris.vogel@me.com>
2024-05-29 20:24:14 +0200

    new eventbus that cleans up, solves restart plugin interface crashing

--- source/EventBus.h
+++ source/EventBus.h
@@ -3,22 +3,53 @@
 #include <functional>
 #include <vector>
 
+#include <functional>
+#include <memory>
+#include <mutex>
+#include <vector>
+
 class ParameterEventBus
 {
 public:
     using EventCallback = std::function<void (int param, int value)>;
+    using CallbackPtr = std::shared_ptr<EventCallback>;
 
-    static void subscribe (EventCallback callback)
+    static CallbackPtr subscribe (EventCallback callback)
     {
-        getInstance().listeners.push_back (callback);
+        auto& instance = getInstance();
+        std::lock_guard<std::mutex> lock (instance.mutex);
+        auto cbPtr = std::make_shared<EventCallback> (callback);
+        instance.listeners.push_back (cbPtr);
+        return cbPtr;
+    }
+
+    static void unsubscribe (CallbackPtr callbackPtr)
+    {
+        auto& instance = getInstance();
+        std::lock_guard<std::mutex> lock (instance.mutex);
+
+        auto it = std::remove_if (instance.listeners.begin(), instance.listeners.end(), [&callbackPtr] (const std::weak_ptr<EventCallback>& weakCb) {
+            if (auto sharedCb = weakCb.lock())
+            {
+                return sharedCb == callbackPtr;
+            }
+            return false;
+        });
+
+        instance.listeners.erase (it, instance.listeners.end());
     }
 
     static void publish (int param, int value)
     {
         auto& instance = getInstance();
-        for (auto& callback : instance.listeners)
+        std::lock_guard<std::mutex> lock (instance.mutex);
+
+        for (auto& weakCb : instance.listeners)
         {
-            callback (param, value);
+            if (auto cb = weakCb.lock())
+            {
+                (*cb) (param, value);
+            }
         }
     }
 
@@ -31,5 +62,6 @@ private:
         return instance;
     }
 
-    std::vector<EventCallback> listeners;
+    std::vector<std::weak_ptr<EventCallback>> listeners;
+    std::mutex mutex;
 };
